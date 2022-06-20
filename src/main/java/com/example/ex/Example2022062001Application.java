0bb290e87fa21c62dd7530446e25b9523370c0f7
ktaekim2 <ktaekim2@gmail.com>
2022-06-20 10:34:44 +0900

    first commit

---
+++ src/main/java/com/example/ex/Example2022062001Application.java
@@ -0,0 +1,13 @@
+package com.example.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example2022062001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example2022062001Application.class, args);
+    }
+
+}
